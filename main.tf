279527e22121e07febbf61abf9b688e4f0d6d65f
vnnimbalkar <vaibhav.nimbalkar291@gmail.com>
2024-01-21 19:57:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,28 +2,3 @@ provider "google" {
   project     = var.project
   region      = var.region
 }
-
-resource "google_compute_instance" "default" {
-  name         = var.instance_name
-  machine_type = "e2-micro"
-  
-  zone         = var.zone
-  tags = ["dev", "engineering"]
-
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        env = "dev"
-      }
-    }
-  }
-
-  network_interface {
-    network = "default"
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-}
-
